05f1c070e9abb2629e901b06d09daf6c4ce44a1d
nika <pinskaya.nika@mail.ru>
2019-11-08 19:30:43 +0300

    Scripted auto-commit on change (2019-11-08 19:30:43) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/StorageResultsActor.java
+++ lab4/src/main/java/com/examples/testjs/StorageResultsActor.java
@@ -10,12 +10,13 @@ public class StorageResultsActor extends AbstractActor {
     private Map<String, String> store = new HashMap<>();
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(StoreMessage.class, m -> {
-            store.put(m.getKey(), m.getValue());
-            System.out.println("receive message! " + m.toString());
-        })
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! " + m.toString());
+                })
                 .match(GetMessage.class, req -> sender().tell(
-                        new StoreMessage(req.getKey(), store.get(req.getKey)))
-                ))
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+        ).build();
     }
 }
