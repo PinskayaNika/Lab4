4f2469eea24cc679011aa7bb5d9052a182e1e22d
nika <pinskaya.nika@mail.ru>
2019-11-08 19:28:35 +0300

    Scripted auto-commit on change (2019-11-08 19:28:35) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/StorageResultsActor.java
+++ lab4/src/main/java/com/examples/testjs/StorageResultsActor.java
@@ -1,4 +1,21 @@
 package com.examples.testjs;
 
-public class StorageResultsActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StorageResultsActor extends AbstractActor {
+    private Map<String, String> store = new HashMap<>();
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create().match(StoreMessage.class, m -> {
+            store.put(m.getKey(), m.getValue());
+            System.out.println("receive message! " + m.toString());
+        })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey))
+                ))
+    }
 }
