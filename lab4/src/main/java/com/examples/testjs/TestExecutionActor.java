4e8001c840e40218976803238381af72fc2fe086
nika <pinskaya.nika@mail.ru>
2019-11-08 23:47:53 +0300

    Scripted auto-commit on change (2019-11-08 23:47:53) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/TestExecutionActor.java
+++ lab4/src/main/java/com/examples/testjs/TestExecutionActor.java
@@ -1,4 +1,12 @@
 package com.examples.testjs;
 
+
+//актор который исполняет один тест из пакета.
+//   Для исполнения JS кода можно воспользоваться следующим примером
+//ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+//engine.eval(jscript);
+//Invocable invocable = (Invocable) engine;
+//return invocable.invokeFunction(functionName, params).toString();
+
 public class TestExecutionActor {
 }
