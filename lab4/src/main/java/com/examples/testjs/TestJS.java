db24a74a9a3b5d2eec0e8b620c948ed8653e5050
nika <pinskaya.nika@mail.ru>
2019-11-16 01:13:36 +0300

    Scripted auto-commit on change (2019-11-16 01:13:36) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/TestJS.java
+++ lab4/src/main/java/com/examples/testjs/TestJS.java
@@ -24,17 +24,19 @@ public class TestJS {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        MainHttp instanse = new MainHttp(system);
+        JSAkkaTester app = new JSAkkaTester();
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                instanse.createRoute(system).flow(system, materializer);
+                app.JSTesterRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
                 );
+
         System.out.println("Server online at http://localhost:8080/\\nPress RETURN to stop...");
         System.in.read();
+
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
