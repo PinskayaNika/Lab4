b006421f71d0118ef4caae2dbc89461463b447ec
nika <pinskaya.nika@mail.ru>
2019-11-15 19:08:48 +0300

    Scripted auto-commit on change (2019-11-15 19:08:48) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/TestJS.java
+++ lab4/src/main/java/com/examples/testjs/TestJS.java
@@ -4,6 +4,6 @@ import akka.actor.ActorSystem;
 
 public class TestJS {
     public static void main(String[] args) throws Exception {
-        ActorSystem system = ActorSystem
+        ActorSystem system = ActorSystem.create("test")
     }
 }
