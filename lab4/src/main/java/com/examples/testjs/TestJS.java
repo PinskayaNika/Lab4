07943c6f9038fb5bd96e8971783b66e7290e73c2
nika <pinskaya.nika@mail.ru>
2019-11-15 19:11:28 +0300

    Scripted auto-commit on change (2019-11-15 19:11:28) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/TestJS.java
+++ lab4/src/main/java/com/examples/testjs/TestJS.java
@@ -1,9 +1,17 @@
 package com.examples.testjs;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class TestJS {
     public static void main(String[] args) throws Exception {
-        ActorSystem system = ActorSystem.create("test")
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class)
+        );
+        storeActor.tell(
+                new StoreActor.StoreMessage("test", "test"),
+                ActorRef.noSender()
+        );
     }
 }
