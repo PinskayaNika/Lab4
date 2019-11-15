8277b8844549bc6649a4945fc0032777697275d0
nika <pinskaya.nika@mail.ru>
2019-11-15 22:22:07 +0300

    Scripted auto-commit on change (2019-11-15 22:22:07) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/StoreActor.java
+++ lab4/src/main/java/com/examples/testjs/StoreActor.java
@@ -1,4 +1,18 @@
 package com.examples.testjs;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.routing.RoundRobinPool;
+
+public class StoreActor extends AbstractActor {
+    private final static int NUM_ROUND_ROBIN_POOL = 5;  //показывает сколько executors может быть создано
+
+    private final ActorRef executors;  //максимально может быть создано NUM_ROUND_ROBIN_POOL раз
+    private final ActorRef storage;  //может быть только 1
+
+    public StoreActor() {
+        executors = getContext().actorOf(new RoundRobinPool(NUM_ROUND_ROBIN_POOL)
+                .props(Props.create(JSExecActor.)))
+    }
 }
