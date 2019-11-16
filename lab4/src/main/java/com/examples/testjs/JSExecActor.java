7df425d4c75dde57a2cf970f06a8d2a8e706f37a
nika <pinskaya.nika@mail.ru>
2019-11-16 11:42:11 +0300

    Scripted auto-commit on change (2019-11-16 11:42:11) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/JSExecActor.java
+++ lab4/src/main/java/com/examples/testjs/JSExecActor.java
@@ -2,18 +2,41 @@ package com.examples.testjs;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.japi.Pair;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
 
-public class JSExecActor {
+public class JSExecActor extends AbstractActor{
+    private static final String JS_ENGINE = "nashorn";
+    private static final String WRONG_ANSWER = "WRONG ANSWER!";
+    private static final String CORRECT_ANSWER = "CORRECT ANSWER!";
 
     @Override
-    public AbstractActor.Receive createReceive() {
+    public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(MessageProcessingActor.class, req ->
+                .match(ExecuteMessage.class, m -> {
+                    Pair<Integer, FunctionPackage> msg = m.getMsg();
+                    int index = msg.getKey();
+                    FunctionPackage functionPackage = msg.getValue();
+                    Test test = functionPackage.getTests()[index];
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_ENGINE);
+                    try {
+                        engine.eval(functionPackage.getJsScript());
+                    } catch (ScriptException e) {
+                        e.printStackTrace();
+                    }
+
+                    Invocable invocable = (Invocable) engine;
+                    String res = invocable.invokeFunction(functionPackage.getFunctionName(),
+                            test.getParams()).toString();
+
                         getSender().tell(
-                                store.get(req.getPackageId()).toArray(),
+                                store.get(m.getPackageId()).toArray(),
                                 ActorRef.noSender()
                         )
                 )
