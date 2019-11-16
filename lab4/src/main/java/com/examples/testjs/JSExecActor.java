c90e68574a9a22b7feb3b2a8d8ddd7288b0d365a
nika <pinskaya.nika@mail.ru>
2019-11-16 11:31:19 +0300

    Scripted auto-commit on change (2019-11-16 11:31:19) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/JSExecActor.java
+++ lab4/src/main/java/com/examples/testjs/JSExecActor.java
@@ -1,4 +1,33 @@
 package com.examples.testjs;
 
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+
 public class JSExecActor {
+
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(MessageProcessingActor.class, req ->
+                        getSender().tell(
+                                store.get(req.getPackageId()).toArray(),
+                                ActorRef.noSender()
+                        )
+                )
+                .match(StoreComand.class, msg -> {
+                            if (store.containsKey(msg.getPackageId())) {
+                                ArrayList<StoreMessage> tests = store.get(msg.getPackageId());
+                                tests.add(msg.getStorageMessage());
+                                store.put(msg.getPackageId(), tests);
+                            } else {
+                                ArrayList<StoreMessage> tests = new ArrayList<>();
+                                tests.add(msg.getStorageMessage());
+                                store.put(msg.getPackageId(), tests);
+                            }
+                        }
+                ).build();
+    }
 }
