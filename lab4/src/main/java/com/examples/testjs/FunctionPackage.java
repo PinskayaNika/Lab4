19e5feba3e76bd60885238ad2b9e849868f49f92
nika <pinskaya.nika@mail.ru>
2019-11-22 18:47:26 +0300

    Scripted auto-commit on change (2019-11-22 18:47:26) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/FunctionPackage.java
+++ lab4/src/main/java/com/examples/testjs/FunctionPackage.java
@@ -1,14 +1,63 @@
 package com.examples.testjs;
+//
+//import akka.actor.ActorRef;
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//public class FunctionPackage {
+//    private static final String PACKAGE_ID = "packageId";
+//    private static final String JS_SCRIPT = "Script";
+//    private static final String FUNCTION_NAME = "functionName";
+//    private static final String TESTS = "tests";
+//
+//    private final int packageId;
+//    private final String jsScript;
+//    private final String functionName;
+//    private final TestExecutionActor[] tests;
+//
+//    @JsonCreator
+//    public FunctionPackage(@JsonProperty(PACKAGE_ID) String packageId,
+//                           @JsonProperty(JS_SCRIPT) String jsScript,
+//                           @JsonProperty(FUNCTION_NAME) String functionName,
+//                           @JsonProperty(TESTS) TestExecutionActor[] tests) {
+//        this.packageId = Integer.parseInt(packageId);
+//        this.jsScript = jsScript;
+//        this.functionName = functionName;
+//        this.tests = tests;
+//    }
+//
+//    String getJsScript() {
+//        return jsScript;
+//    }
+//
+//    int getPackageId() {
+//        return packageId;
+//    }
+//
+//    String getFunctionName() {
+//        return functionName;
+//    }
+//
+//    TestExecutionActor[] getTests() {
+//        return tests;
+//    }
+//
+//    public TestExecutionActor getTests(int i) {
+//        return tests[i];
+//    }
+//}
 
-import akka.actor.ActorRef;
+
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
 public class FunctionPackage {
-    private static final String PACKAGE_ID = "packageId";
-    private static final String JS_SCRIPT = "Script";
-    private static final String FUNCTION_NAME = "functionName";
-    private static final String TESTS = "tests";
+    private final static String PACKAGE_ID = "packageId";
+    private final static String JS_SCRIPT = "jsScript";
+    private final static String FUNCTION_NAME = "functionName";
+    private final static String TESTS = "tests";
 
     private final int packageId;
     private final String jsScript;
@@ -26,23 +75,23 @@ public class FunctionPackage {
         this.tests = tests;
     }
 
-    String getJsScript() {
+    public String getJSScript() {
         return jsScript;
     }
 
-    int getPackageId() {
+    public int getPackageId() {
         return packageId;
     }
 
-    String getFunctionName() {
+    public String getFunctionName() {
         return functionName;
     }
 
-    TestExecutionActor[] getTests() {
+    public TestExecutionActor[] getTests() {
         return tests;
     }
 
-    public TestExecutionActor getTests(int i) {
+    public TestExecutionActor getTest(int i) {
         return tests[i];
     }
 }
