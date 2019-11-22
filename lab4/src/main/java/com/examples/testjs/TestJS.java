dc09940596c895d149cf4d3a4bbb7b4d6c90cddf
nika <pinskaya.nika@mail.ru>
2019-11-22 18:43:08 +0300

    Scripted auto-commit on change (2019-11-22 18:43:08) by gitwatch.sh

--- lab4/src/main/java/com/examples/testjs/TestJS.java
+++ lab4/src/main/java/com/examples/testjs/TestJS.java
@@ -27,15 +27,22 @@ import java.util.concurrent.CompletionStage;
 
 public class TestJS extends AllDirectives {
 
+//    static ActorRef mainActor;
+//    private static final String LOCALHOST = "localhost:";
+//    private static final String SERVER_INFO = "Server online at http://localhost:8080/\\nPress RETURN to stop...";
+//    private static final String PACKAGE_ID = "packageId";
+//    private static final String POST_MESSAGE = "Message was posted";
+//    private static final int SERVER_PORT = 8080;
+//    private static final int TIMEOUT_MILLIS = 5000;
+
     static ActorRef mainActor;
-    private static final String LOCALHOST = "localhost:";
-    private static final String SERVER_INFO = "Server online at http://localhost:8080/\\nPress RETURN to stop...";
+    private static final String ROUTES = "routes";
+    private static final String LOCALHOST = "localhost";
+    private static final String SERVER_INFO = "Server online on localhost:8080/\n PRESS ANY KEY TO STOP";
     private static final String PACKAGE_ID = "packageId";
     private static final String POST_MESSAGE = "Message was posted";
     private static final int SERVER_PORT = 8080;
     private static final int TIMEOUT_MILLIS = 5000;
-
-
 
     public static void main(String[] args) throws Exception {
         //Инициализация сервера
@@ -73,32 +80,7 @@ public class TestJS extends AllDirectives {
     }
 
 
-//    private Route jsTesterRoute() {
-//        return concat(
-//                get(
-//                        () -> parameter(PACKAGE_ID, (packageId) ->
-//                                {
-//                                    Future<Object> result = Patterns.ask(mainActor,
-//                                            new MessageProcessingActor(Integer.parseInt(packageId)),
-//                                            TIMEOUT_MILLIS);
-//                                    return completeOKWithFuture(result, Jackson.marshaller());
-//                                }
-//                        )
-//                ),
-//                post(
-//                        () -> entity(Jackson.unmarshaller(FunctionPackage.class),
-//                                msg -> {
-//                                    mainActor.tell(msg, ActorRef.noSender());
-//                                    return complete(POST_MESSAGE);
-//                                }
-//                        )
-//                )
-//        );
-//
-//    }
-
     private Route jsTesterRoute() {
-
         return concat(
                 get(
                         () -> parameter(PACKAGE_ID, (packageId) ->
@@ -115,8 +97,10 @@ public class TestJS extends AllDirectives {
                                 msg -> {
                                     mainActor.tell(msg, ActorRef.noSender());
                                     return complete(POST_MESSAGE);
-                                })));
+                                }
+                        )
+                )
+        );
+
     }
-
-
 }
